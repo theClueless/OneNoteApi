85e3b6b351150371e6c5fe235a6b5709587a8ec3
a <t>
2022-02-26 01:09:24 +0200

    add basic section task crawler

---
+++ OneNoteApi.Specific/SectionCrawler/SectionTaskCrawler.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OneNoteApi.Hierarchy;
+using OneNoteApi.PageContent;
+
+namespace OneNoteApi.Mine.SectionCrawler
+{
+    public class SectionTaskCrawler
+    {
+        private readonly IOneNote _oneNoteServices;
+        private readonly string _sectionId;
+        public SectionState State { get; } = new();
+        public double AvgTime { get; private set; }
+        private int _updateAmount = -1;
+
+        public SectionTaskCrawler(IOneNote oneNoteServices, string sectionId)
+        {
+            _oneNoteServices = oneNoteServices;
+            _sectionId = sectionId;
+        }
+
+        public void Update()
+        {
+            var sw = Stopwatch.StartNew();
+            var pages2 = _oneNoteServices.PageHierarchyService.GetPages(_sectionId).ToList();
+
+            var changed = pages2.Where(x =>
+            {
+                if (State.Pages.TryGetValue(x.Id, out var page))
+                {
+                    return page.LastModifiedTime != x.LastModifiedTime;
+                }
+
+                return true;
+            }).ToList();
+            foreach (var page in changed)
+            {
+                var content = _oneNoteServices.PageContentService.GetPageContent(page);
+                var tasks = GetOeTasksFromPage(content);
+                State.Pages[page.Id] = page;
+                State.PagesToTasks[page.Id] = tasks;
+            }
+
+            sw.Stop();
+            if (_updateAmount == -1)
+            {
+                _updateAmount++;
+                return;
+            }
+            AvgTime = (sw.Elapsed.TotalMilliseconds + AvgTime * _updateAmount++) / _updateAmount;
+        }
+
+        private static List<OE> GetOeTasksFromPage(Page page)
+        {
+            List<OE> pageOe = new List<OE>();
+            foreach (var pageOutline in page.Outlines)
+            {
+                foreach (var pageOutlineChild in pageOutline.Children)
+                {
+                    GetOeTasksFromPage(pageOutlineChild, pageOe);
+                }
+            }
+
+            return pageOe;
+        }
+
+        private static void GetOeTasksFromPage(OE oe, List<OE> aggregatedList)
+        {
+            var tag = oe.Tag;
+            if (tag.Exists && !tag.IsCompleted && tag.TagType == KnownTags.ToDoTagIndex)
+            {
+                aggregatedList.Add(oe);
+                return;
+            }
+
+            if (oe.HasChildren)
+            {
+                foreach (var child in oe.Children)
+                {
+                    GetOeTasksFromPage(child, aggregatedList);
+                }
+            }
+        }
+    }
+
+    public class SectionState
+    {
+        /// <summary>
+        /// from id to the pages model
+        /// </summary>
+        public Dictionary<string, PageHierarchyModel> Pages { get; set; } = new();
+
+        public Dictionary<string, List<OE>> PagesToTasks { get; set; } = new();
+    }
+}
--- OneNoteApiConsole/Program.cs
+++ OneNoteApiConsole/Program.cs
@@ -1,9 +1,14 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Threading;
 using OneNoteApi;
 using OneNoteApi.Common;
 using OneNoteApi.Hierarchy;
 using OneNoteApi.Mine;
+using OneNoteApi.Mine.SectionCrawler;
+using OneNoteApi.PageContent;
 
 namespace OneNoteApiConsole
 {
@@ -11,12 +16,20 @@ namespace OneNoteApiConsole
     {
         static void Main(string[] args)
         {
+            TryCrawler();
             var one = new OneNote();
-            // var toSort = one.PageHierarchyService.GetPages().FirstOrDefault(x => x.Name == "To Sort"); // {1362B548-DA66-0234-1438-D61F6A4096C3}{1}{E17810630182128276241987605806990932121981}
-            var tosortHandler = new ToSortPageHandler(one); //@"{1362B548-DA66-0234-1438-D61F6A4096C3}{1}{E17810630182128276241987605806990932121981}");
-            // tosortHandler.AddNewTask("this is another new task");
-            tosortHandler.ArchiveCompletedTasks();
-            
+            var sw = Stopwatch.StartNew();
+            // get all projects pages
+            CrawlerPrep(one, sw);
+
+
+            //// var toSort = one.PageHierarchyService.GetPages().FirstOrDefault(x => x.Name == "To Sort"); // {1362B548-DA66-0234-1438-D61F6A4096C3}{1}{E17810630182128276241987605806990932121981}
+            //var tosortHandler = new ToSortPageHandler(one); //@"{1362B548-DA66-0234-1438-D61F6A4096C3}{1}{E17810630182128276241987605806990932121981}");
+            //var page = one.PageContentService.GetPageContent();
+            //page.Outlines.First().Children.First().
+            //// tosortHandler.AddNewTask("this is another new task");
+            //tosortHandler.ArchiveCompletedTasks();
+
             //var content = one.PageContentService.GetPageContent(toSort);
             //var xml = content.Root;
             //     one.PageHierarchyService.AddPageToSection("{7DAC32A7-AD8B-FA3F-36C8-FD4777D1896F}{1}{B0}", page);
@@ -35,6 +48,113 @@ namespace OneNoteApiConsole
 
             PlayWithTodayUpdater();
         }
+
+        private static void CrawlerPrep(OneNote one, Stopwatch sw)
+        {
+            var pages = one.PageHierarchyService.GetPages(@"{82AC11DF-DCB7-069E-1E51-00FB2B5D48FF}{1}{B0}").ToList();
+
+
+            Dictionary<PageHierarchyModel, List<OE>> pageToTasks = new();
+
+            // get all tasks
+            foreach (var page in pages)
+            {
+                var content = one.PageContentService.GetPageContent(page);
+                var tasks = GetOeTasksFromPage(content);
+                if (tasks.Count > 0)
+                {
+                }
+
+                pageToTasks[page] = tasks;
+            }
+
+            sw.Stop();
+
+            Console.WriteLine($"Got Pages: {pages.Count}");
+            Console.WriteLine($"Took: {sw.Elapsed.TotalSeconds}");
+
+            foreach (var (p, task) in pageToTasks)
+            {
+                if (task.Any())
+                {
+                    Console.WriteLine($"{p.Name}: {task.Count} tasks");
+                    foreach (var oe in task)
+                    {
+                        Console.WriteLine($"    {oe.Text.Value}");
+                    }
+                }
+            }
+
+            // recheck - 
+            Dictionary<string, PageHierarchyModel> idToPages = pages.ToDictionary(x => x.Id);
+            var pages2 = one.PageHierarchyService.GetPages(@"{82AC11DF-DCB7-069E-1E51-00FB2B5D48FF}{1}{B0}").ToList();
+            var changed = pages2.Where(x => idToPages[x.Id].LastModifiedTime != x.LastModifiedTime).ToList();
+            foreach (var page in changed)
+            {
+                var content = one.PageContentService.GetPageContent(page);
+                var tasks = GetOeTasksFromPage(content);
+                if (tasks.Count > 0)
+                {
+                }
+
+                pageToTasks[page] = tasks;
+            }
+        }
+
+        private static void TryCrawler()
+        {
+            SectionTaskCrawler crawler = new(new OneNote(), @"{82AC11DF-DCB7-069E-1E51-00FB2B5D48FF}{1}{B0}");
+            crawler.Update();
+            Console.WriteLine($"First update: {crawler.AvgTime}" );
+            int i = 0;
+            Stopwatch sw = Stopwatch.StartNew();
+            while (true)
+            {
+                sw.Restart();
+                crawler.Update();
+                sw.Stop();
+                Console.WriteLine($"Current: {sw.ElapsedMilliseconds}, Avg Time: {crawler.AvgTime}");
+                Thread.Sleep(1000);
+                
+            }
+            
+        }
+
+        static List<OE> GetOeTasksFromPage(Page page)
+        {
+            List<OE> pageOe = new List<OE>();
+            foreach (var pageOutline in page.Outlines)
+            {
+                foreach (var pageOutlineChild in pageOutline.Children)
+                {
+                    GetOeTasksFromPage(pageOutlineChild, pageOe);
+                }
+            }
+
+            return pageOe;
+        }
+
+        static void GetOeTasksFromPage(OE oe, List<OE> aggregatedList)
+        {
+            var tag = oe.Tag;
+            if (tag.Exists && !tag.IsCompleted && tag.TagType == KnownTags.ToDoTagIndex)
+            {
+                aggregatedList.Add(oe);
+                return;
+            }
+
+            if (oe.HasChildren)
+            {
+                foreach (var child in oe.Children)
+                {
+                    GetOeTasksFromPage(child, aggregatedList);
+                }
+            }
+        }
+
+
+
+
 
         static void PlayWithTodayUpdater()
         {
